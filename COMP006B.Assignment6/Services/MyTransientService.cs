c13f6f143175e01e8f48009991df59c70e2a8620
Dracston <lpj11101@gmail.com>
2024-04-29 18:27:57 -0700

    Trasient service added

--- COMP006B.Assignment6/Services/MyTransientService.cs
+++ COMP006B.Assignment6/Services/MyTransientService.cs
@@ -2,5 +2,19 @@
 {
     public class MyTransientService
     {
+        //private field
+        private readonly Guid _uniqueId;
+
+        //default constrcutor
+        public MyTransientService()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+
+        //public method that returns the _uniqueId value
+        public Guid GetUniqueID()
+        {
+            return _uniqueId;
+        }
     }
 }
