28512801dbeb89525ccfd3c84a1a3745d20c41a0
Logan Jones <lpj11101@gmail.com>
2024-04-30 00:48:35 -0700

    Singleton Service added

--- COMP006B.Assignment6/Services/MySingletonService.cs
+++ COMP006B.Assignment6/Services/MySingletonService.cs
@@ -2,5 +2,19 @@
 {
     public class MySingletonService
     {
+        //private field 
+        private readonly Guid _uniqueId;
+
+        //default constructor
+        public MySingletonService()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+
+        //public method that returns the _uniqueId value
+        public Guid GetUniqueId()
+        {
+            return _uniqueId;
+        }
     }
 }
