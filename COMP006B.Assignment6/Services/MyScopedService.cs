e114ee961933a9bf40a57117741023a47e0252b4
Logan Jones <lpj11101@gmail.com>
2024-04-30 00:46:16 -0700

    Scoped controller added

--- COMP006B.Assignment6/Services/MyScopedService.cs
+++ COMP006B.Assignment6/Services/MyScopedService.cs
@@ -2,5 +2,20 @@
 {
     public class MyScopedService
     {
+        //private field
+        private readonly Guid _uniqueId;
+
+        //default constructor
+        public MyScopedService()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+        
+        //public method that returns the _uniqueId value
+        public Guid GetUniqueId()
+        {
+            return _uniqueId;
+        }
+
     }
 }
